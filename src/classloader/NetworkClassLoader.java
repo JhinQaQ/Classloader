3dd487967e1cdf1d17ea3f29d5b83887e995ce86
Jjin <jinyang3@iflytek.com>
2019-07-08 10:57:23 +0800

    Initial commit

---
+++ src/classloader/NetworkClassLoader.java
@@ -0,0 +1,56 @@
+package classloader;
+
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.net.URL;
+
+public class NetworkClassLoader extends ClassLoader {
+	
+	private String rootUrl;
+	
+	public NetworkClassLoader(String rootUrl) {
+		this.rootUrl = rootUrl;
+	}
+
+	protected Class<?> findClass(String name) throws ClassNotFoundException {
+		byte[] classData = getClassData(name);
+		if (classData == null) {
+			throw new ClassNotFoundException();
+		}
+		else {
+			return defineClass(name, classData, 0, classData.length);
+		}
+	}
+	
+
+	@Nullable
+	private byte[] getClassData(String className) {
+		String path = classNameToPath(className);
+		try {
+			URL url = new URL(path);
+			InputStream ins = url.openStream();
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			int bufferSize = 4096;
+			byte[] buffer = new byte[bufferSize];
+			int bytesNumRead = 0;
+			while ((bytesNumRead = ins.read(buffer)) != -1) {
+				baos.write(buffer, 0, bytesNumRead);
+			}
+			return baos.toByteArray();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	@NotNull
+	@Contract(pure = true)
+	private String classNameToPath(String className) {
+		return rootUrl + "/"
+				+ className.replace('.', '/') + ".class";
+	}
+}
